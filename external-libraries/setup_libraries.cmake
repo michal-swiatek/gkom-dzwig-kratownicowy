9dd58828d75ba5d653776728cc57257b247a6cdc
Michał Świątek <swiatek.michal006@gmail.com>
2020-05-16 00:07:54 +0200

    Added glfw and glad libraries

---
+++ external-libraries/setup_libraries.cmake
@@ -0,0 +1,24 @@
+#Locate libraries
+set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "${CMAKE_SOURCE_DIR}/cmake/modules/")
+
+# OpenGL
+find_package(OpenGL REQUIRED)
+
+# GLFW
+find_package(GLFW3 REQUIRED)
+message(STATUS "GLFW3 include found at ${GLFW3_INCLUDE_DIR}")
+
+#
+# Build libraries from source files
+#
+
+# GLAD
+set(GLAD_INCLUDE_DIR "${CMAKE_SOURCE_DIR}/external-dependencies/include")
+set(GLAD_SOURCE_DIR "${CMAKE_SOURCE_DIR}/external-dependencies/src")
+add_library(GLAD "${GLAD_SOURCE_DIR}/glad.c")
+
+# Include libraries
+include_directories("${GLFW3_INCLUDE_DIR}" "${GLAD_INCLUDE_DIR}")
+
+# Linking
+set(OPENGL_LIBS "${GLFW3_LIBRARY}" opengl32 GLAD)
