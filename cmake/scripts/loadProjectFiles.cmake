40da92ef216f4439e9a26734a7bb54fa380213dc
Michał Świątek <swiatek.michal006@gmail.com>
2020-05-19 22:04:36 +0200

    Dodałem klasę Shader

--- cmake/scripts/loadProjectFiles.cmake
+++ cmake/scripts/loadProjectFiles.cmake
@@ -1,7 +1,12 @@
 include_directories("${CMAKE_SOURCE_DIR}/include")
 
 set(PROJECT_INCLUDE_FILES "${CMAKE_SOURCE_DIR}/include/Window.h"
-                          "${CMAKE_SOURCE_DIR}/include/Core.h")
+                          "${CMAKE_SOURCE_DIR}/include/Core.h"
+                          "${CMAKE_SOURCE_DIR}/include/Cylinder.h"
+                          "${CMAKE_SOURCE_DIR}/include/Shader.h"
+)
 
 set(PROJECT_SRC_FILES "${CMAKE_SOURCE_DIR}/src/Window.cpp"
-                      "${CMAKE_SOURCE_DIR}/src/Core.cpp")
+                      "${CMAKE_SOURCE_DIR}/src/Core.cpp"
+                      "${CMAKE_SOURCE_DIR}/src/Shader.cpp"
+)
