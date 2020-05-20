2453d543f81c179510546ab1c8c6fb6b7882052e
Michał Świątek <swiatek.michal006@gmail.com>
2020-05-20 02:05:46 +0200

    Dodałem klasę kamery

---
+++ include/Camera.h
@@ -0,0 +1,106 @@
+/*
+ *  Created by michal-swiatek on 20.05.2020, based on Joey de Vries tutorials.
+ *  Gitlab: http://gitlab.ii.pw.edu.pl/gkom.20l/dzwig-kratownicowy
+ *  LearnOpenGL tutorial: https://learnopengl.com/Introduction
+ *
+ *  TODO: implement unit tests
+ */
+
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include <optional>
+
+#include <glm/glm.hpp>
+
+#include "Transform.h"
+
+namespace cam {
+
+    enum class Direction { FORWARD, BACKWARD, RIGHT, LEFT, UP, DOWN };
+    enum class Speed { NORMAL, FAST, SLOW };
+
+    struct Settings
+    {
+        //  Default cam settings
+        static constexpr float MOVEMENT_SPEED = 10.0;
+        static constexpr float ZOOM_SPEED = 0.1;
+        static constexpr float SENSITIVITY = 0.1;
+        static constexpr float FIELD_OF_VIEW = 45.0;
+        static constexpr float ASPECT_RATIO = 4.0 / 3.0;
+
+        float movementSpeed;
+        float zoomSpeed;
+        float sensitivity;
+        float fov;
+        float aspectRatio;
+
+        explicit Settings(float movementSpeed = MOVEMENT_SPEED, float zoomSpeed = ZOOM_SPEED, float sensitivity = SENSITIVITY, float fov = FIELD_OF_VIEW, float aspectRatio = ASPECT_RATIO);
+    };
+
+    struct Orientation
+    {
+        //  Default Euler angles values
+        static constexpr float YAW = 0.0f;
+        static constexpr float PITCH = 0.0f;
+        static constexpr float ROLL = 0.0f;
+
+        //  Default camera rotation
+        static constexpr glm::vec3 ROTATION = glm::vec3(-90.0f, 0.0f, 0.0f);
+
+        //  Default world up vector
+        static constexpr glm::vec3 WORLD_UP = glm::vec3(0.0f, 1.0f, 0.0f);
+
+        glm::vec3 front;
+        glm::vec3 right;
+        glm::vec3 up;
+
+        glm::vec3 worldUp;
+
+        explicit Orientation(const glm::vec3& worldUp);
+    };
+
+    class Camera
+    {
+    public:
+        explicit Camera(const Transform& transform, const glm::vec3& worldUp = Orientation::WORLD_UP);
+        explicit Camera(const glm::vec3& position = glm::vec3(0.0), const glm::vec3& rotation = Orientation::ROTATION, const glm::vec3& worldUp = Orientation::WORLD_UP);
+        Camera(const Settings& settings, const Transform& transform, const glm::vec3& worldUp = Orientation::WORLD_UP);
+        virtual ~Camera() = default;
+
+        [[nodiscard]] glm::mat4 getViewMatrix() const;
+        [[nodiscard]] glm::mat4 getProjectionMatrix() const;
+        [[nodiscard]] glm::mat4 getViewProjectionMatrix() const;
+
+        virtual void move(Direction direction, Speed speed, float deltaTime);
+        virtual void rotate(float yaw, float pitch, float roll);
+        virtual void zoom(float value);
+
+        //  Get
+        Settings& getSettings();  //  Reference for easy manipulation
+
+        [[nodiscard]] float getZoom() const;
+
+        [[nodiscard]] const Transform& getTransform() const;
+        [[nodiscard]] const Orientation& getOrientation() const;
+
+        //  Set
+        void setZoom(float zoom = Settings::FIELD_OF_VIEW);
+
+        void setTransform(const Transform& transform);
+        void setTransform(const glm::vec3& position, const glm::vec3& rotation = Orientation::ROTATION);
+
+        void setWorldUp(const glm::vec3& worldUp = Orientation::WORLD_UP);
+
+    private:
+        Settings settings;
+
+        Transform transform;
+        Orientation orientation;
+
+        // Calculates the front vector from the Camera's (updated) Euler Angles
+        void updateOrientation();
+    };
+}
+
+#endif //CAMERA_H
