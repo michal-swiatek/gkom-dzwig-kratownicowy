948b3154993a8ba4bcfbae1d4f40847db212bbcf
MateuszZalewsky <MateuszZalewsky@gmail.com>
2020-06-04 22:16:54 +0200

    Dodałem SOIL do cmake

--- cmake/scripts/setupLibraries.cmake
+++ cmake/scripts/setupLibraries.cmake
@@ -12,6 +12,10 @@ message(STATUS "GLFW3 include found at ${GLFW3_INCLUDE_DIR}")
 find_package(GLM REQUIRED)
 message(STATUS "GLM include found at ${GLM_INCLUDE_DIRS}")
 
+# SOIL
+set(SOIL_INCLUDE_DIR "${CMAKE_SOURCE_DIR}/external-libraries/include")
+set(SOIL_LIBRARY "${CMAKE_SOURCE_DIR}/external-libraries/lib")
+
 #
 # Build libraries from source files
 #
@@ -22,7 +26,7 @@ set(GLAD_SOURCE_DIR "${CMAKE_SOURCE_DIR}/external-libraries/src")
 add_library(GLAD "${GLAD_SOURCE_DIR}/glad.c")
 
 # Include libraries
-include_directories("${GLFW3_INCLUDE_DIR}" "${GLAD_INCLUDE_DIR}" "${GLM_INCLUDE_DIRS}")
+include_directories("${SOIL_INCLUDE_DIR}" "${GLFW3_INCLUDE_DIR}" "${GLAD_INCLUDE_DIR}" "${GLM_INCLUDE_DIRS}")
 
 # Linking
-set(OPENGL_LIBS "${GLFW3_LIBRARY}" opengl32 GLAD)
+set(OPENGL_LIBS "${GLFW3_LIBRARY}" SOIL opengl32 GLAD)
