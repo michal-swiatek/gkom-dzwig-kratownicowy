1e7bf906a538a68c37db4e15da6cc34986651054
MateuszZalewsky <MateuszZalewsky@gmail.com>
2020-06-07 20:26:04 +0200

    Dodałem gettery, settery

--- include/LightHandler.h
+++ include/LightHandler.h
@@ -44,7 +44,7 @@ private:
     GLfloat constant;
     GLfloat linear;
     GLfloat quadratic;
-    std::unique_ptr<LightSource> lightSource;
+    std::unique_ptr<Cylinder> lightSource;
 
 public:
     PointLight(const glm::vec3& position,const glm::vec3& ambient,
@@ -53,30 +53,43 @@ public:
                const GLfloat& quadratic): position(position),
                                           ambient(ambient),diffuse(diffuse),specular(specular),
                                           constant(constant),linear(linear),quadratic(quadratic),
-                                          lightsrc(std::make_unique<LightSource>(position, 0.5f))
+                                          lightSource(std::make_unique<Cylinder>(position, 0.5f))
     {}
 
     void move(const glm::vec3& displacement);
-    void drawLightSrc(std::shared_ptr<Shader> shader);
+    void drawLightSource();
+    const glm::vec3 &getPosition() const;
+    void setPosition(const glm::vec3 &position);
+    const glm::vec3 &getAmbient() const;
+    void setAmbient(const glm::vec3 &ambient);
+    const glm::vec3 &getDiffuse() const;
+    void setDiffuse(const glm::vec3 &diffuse);
+    const glm::vec3 &getSpecular() const;
+    void setSpecular(const glm::vec3 &specular);
+    GLfloat getConstant() const;
+    void setConstant(GLfloat constant);
+    GLfloat getLinear() const;
+    void setLinear(GLfloat linear);
+    GLfloat getQuadratic() const;
+    void setQuadratic(GLfloat quadratic);
 };
 
-class LightManager {
+class LightHandler {
 private:
     const static int MAX_POINT_LIGHTS = 10;
-    std::shared_ptr<Shader> lightSrcShader;
+    std::shared_ptr<Shader> lightSourceShader;
     std::unique_ptr<DirectionalLight> dirLight;
-    std::vector<std::shared_ptr<PointLight>> pointLight;
+    std::vector<std::shared_ptr<PointLight>> pointLights;
 
     cam::Camera camera;
 
 public:
-    LightManager(const cam::Camera& camera)
-    {
-        this->camera = camera;
-        dirLight = std::make_unique<DirectionalLight>(DirectionalLight());
-    }
-    void movePointLight(glm::vec3 displacement);
+    explicit LightHandler(const cam::Camera& camera);
+    void movePointLight(glm::vec3 displacement, uint8_t target);
     void applyLightToShader(std::shared_ptr<Shader> shader);
+
+    void setDirLight(glm::vec3 direction, glm::vec3 ambient,
+                     glm::vec3 diffuse,glm::vec3 specular);
 };
 
 #endif //DZWIG_KRATOWNICOWY_LIGHTHANDLER_H
--- src/LightHandler.cpp
+++ src/LightHandler.cpp
@@ -35,3 +35,99 @@ const glm::vec3 &DirectionalLight::getSpecular() const {
 void DirectionalLight::setSpecular(const glm::vec3 &specular) {
     DirectionalLight::specular = specular;
 }
+
+LightHandler::LightHandler(const cam::Camera &camera) {
+    this->camera = camera;
+    dirLight = std::make_unique<DirectionalLight>(DirectionalLight());
+}
+
+void LightHandler::movePointLight(glm::vec3 displacement, uint8_t target) {
+    pointLights[target]->move(displacement);
+}
+
+void LightHandler::applyLightToShader(std::shared_ptr<Shader> shader) {
+    shader->use();
+    shader->setVector3f("eyePos", camera.getTransform().position);
+    shader->setVector3f("dirLight.direction", dirLight->getDirection());
+    shader->setVector3f("dirLight.ambient", dirLight->getAmbient() + 0.15f );
+    shader->setVector3f("dirLight.diffuse", dirLight->getDiffuse());
+    shader->setVector3f("dirLight.specular", dirLight->getSpecular());
+
+    for(auto pointLight:pointLights) {
+        shader->use();
+        shader->setVector3f("pointLight.position", pointLight->getPosition());
+        shader->setVector3f("pointLight.ambient", pointLight->getAmbient());
+        shader->setVector3f("pointLight.diffuse", pointLight->getDiffuse());
+        shader->setVector3f("pointLight.specular", pointLight->getSpecular());
+        shader->setFloat("pointLight.constant", pointLight->getConstant());
+        shader->setFloat("pointLight.linear", pointLight->getLinear());
+        shader->setFloat("pointLight.quadratic", pointLight->getQuadratic());
+        lightSourceShader->use();
+        pointLight->drawLightSource();
+    }
+
+}
+
+void LightHandler::setDirLight(glm::vec3 direction, glm::vec3 ambient,
+        glm::vec3 diffuse,glm::vec3 specular) {
+    dirLight->setDirection(direction);
+    dirLight->setAmbient(ambient);
+    dirLight->setDiffuse(diffuse);
+    dirLight->setSpecular(specular);
+}
+
+const glm::vec3 &PointLight::getPosition() const {
+    return position;
+}
+
+void PointLight::setPosition(const glm::vec3 &position) {
+    PointLight::position = position;
+}
+
+const glm::vec3 &PointLight::getAmbient() const {
+    return ambient;
+}
+
+void PointLight::setAmbient(const glm::vec3 &ambient) {
+    PointLight::ambient = ambient;
+}
+
+const glm::vec3 &PointLight::getDiffuse() const {
+    return diffuse;
+}
+
+void PointLight::setDiffuse(const glm::vec3 &diffuse) {
+    PointLight::diffuse = diffuse;
+}
+
+const glm::vec3 &PointLight::getSpecular() const {
+    return specular;
+}
+
+void PointLight::setSpecular(const glm::vec3 &specular) {
+    PointLight::specular = specular;
+}
+
+GLfloat PointLight::getConstant() const {
+    return constant;
+}
+
+void PointLight::setConstant(GLfloat constant) {
+    PointLight::constant = constant;
+}
+
+GLfloat PointLight::getLinear() const {
+    return linear;
+}
+
+void PointLight::setLinear(GLfloat linear) {
+    PointLight::linear = linear;
+}
+
+GLfloat PointLight::getQuadratic() const {
+    return quadratic;
+}
+
+void PointLight::setQuadratic(GLfloat quadratic) {
+    PointLight::quadratic = quadratic;
+}
